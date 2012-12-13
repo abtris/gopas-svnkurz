5af3c42b072400f735221a318632092bbc85f8e2
Ladislav Prskavec <ladislav@prskavec.net>
2012-12-13 11:37:47 +0100

    Update scripts

--- Day2/Uloha-1/install.sh
+++ Day2/Uloha-1/install.sh
@@ -1,27 +1,22 @@
 sudo apt-get install vim mc tree
 sudo apt-get install apache2 libapache2-svn
 
-# make Repos and structure
-mkdir -p /home/student/Gopas/REPOS
-mkdir -p /home/student/Gopas/WC
-svnadmin create /home/student/Gopas/REPOS/Team1
-svnadmin create /home/student/Gopas/REPOS/Team2
-svn co file:///home/student/Gopas/REPOS/Team1
-cd /home/student/Gopas/REPOS/Team1
+# make repos and structure
+mkdir -p /home/student/gopas/repos
+mkdir -p /home/student/gopas/wc
+svnadmin create /home/student/gopas/repos/Team1
+svnadmin create /home/student/gopas/repos/Team2
+mkdir -p /home/student/gopas/wc/xxxx
+cd /home/student/gopas/wc/xxxx
 mkdir tags trunk branches
-svn add tags trunk branches
-svn commit -m "Added structure"
-mkdir -p /home/student/Gopas/WC/xxxx
-cd /home/student/Gopas/WC/xxxx
-mkdir tags trunk branches
-svn import file:///home/student/Gopas/REPOS/Team2 -m "Added Structure"
+svn import file:///home/student/gopas/repos/Team1 -m "Added Structure"
+svn import file:///home/student/gopas/repos/Team2 -m "Added Structure"
 
 # make Right
-# sudo vim /etc/apache2/modes-available/dav_svn.conf 
-# sudo vim  /etc/apache2/dav_svn.authz
 sudo htpasswd -bc /etc/apache2/dav_svn.passwd Petr heslo
 sudo htpasswd -b /etc/apache2/dav_svn.passwd Jan heslo
 sudo htpasswd -b /etc/apache2/dav_svn.passwd Lada heslo
 sudo htpasswd -b /etc/apache2/dav_svn.passwd Pavel heslo
-# cat /etc/apache2/dav_svn.passwd
+sudo cp dav_svn.authz /etc/apache2/dav_svn.authz
+sudo cp dav_svn.conf /etc/apache2/modes-available/dav_svn.conf
 sudo /etc/init.d/apache2 restart
